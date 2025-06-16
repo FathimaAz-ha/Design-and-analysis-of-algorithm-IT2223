40217a394b70827baabaab670910a2606bbf7897
Fathima Azha <2021ict48@stu.vau.ac.lk>
2025-06-16 16:29:38 +0530

    Update tree_unweight.m

--- Tree-and-Tree-Traversal/tree_unweight.m
+++ Tree-and-Tree-Traversal/tree_unweight.m
@@ -4,11 +4,9 @@ nodes = {'A','B','C','D','E','F','G'};
 s = [1 1 2 2 3 3];
 t = [2 3 4 5 6 7];
 
-%define edge weights
-weights = [5 3 2 4 6 1];
 
 %create a directed graph
-G = digraph(s,t,weights, nodes);
+G = digraph(s,t,[], nodes);
 
 %plot the tree
 plot(G, 'Layout','layered', 'Direction', 'down','EdgeLabel', G.Edges.Weight);
